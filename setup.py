74d33221fdcb9a042011729f7faf21876f23183b
Adrien Duclos <adrien.duclos@milibris.com>
2015-12-21 18:50:05 +0100

    Setup script

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='Groggy',
+      version='1.0',
+      description='A framework for roguelike games',
+      author='Raveline',
+      packages=find_packages())
