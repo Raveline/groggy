d8a58f6abc823fa4e69becb603b6a4cd6ebe6a88
Adrien Duclos <adrien.duclos@milibris.com>
2015-12-21 17:54:48 +0100

    Better abstraction for states

--- groggy/ui/state.py
+++ groggy/ui/state.py
@@ -4,90 +4,109 @@ States are a core concept of the Groggy framework. A State is a situation
 in a game, that determinates the result of inputs, the things to display,
 and so on.
 
-States are dynamically build from "Trees", namely dictionaries.
-
 Game comes from one state to the other in the fashion of a stack.
 E.g. : Main menu -> Main Game State -> A Game submenu, all those three
 states will stack; if one is left, game will come back to the previous
 state.
+
+There are mainly two types of states:
+    - GameStates are there to display a world, and handle interactions
+    with this world. This typically means that a GameState should be able
+    to build some Command pattern and send it through the event bus.
+    - MenuStates handle the ugly world of windows, desktop-based ui, and so
+    on.
+
+States behaviour are dynamically build from "Trees", namely dictionaries.
+This dictionnary would typically include the following information:
+    - 'name' for the name of the state, used for debugging purposes.
+    - If this is a substate, it probably has an 'action', namely a main
+    action that will be used to build the related command. E.g., launching
+    a spell in a typicall roguelike, putting an object somewhere in a
+    management game, etc.
+    - If this is more a "general state" (typically the main game state), it
+    exposes an "actions" dictionnary (note the plural) that tells in what kind
+    of substates one can enter. Note that those actions are supposed to be
+    states, and if called, will launch the "NEW_STATE" event and stack a state.
+    Changing state (and building the proper one) is a responsability of the
+    Game class.
+    - Finally, a "pauses_game" flag should tell if the game is paused when
+    in this state or if real-time display should still be on.
 """
 from groggy.utils import bus
-from groggy.utils.dict_path import read_path_dict
-from groggy.ui.component_builder import make_questionbox
 from groggy.inputs.input import Inputs
 
 NEW_STATE = 0
 
 
+class InvalidTreeException(Exception):
+    pass
+
+
 class GameState(object):
-    def __init__(self, state_tree, navigator=None, parent_state=None):
+    def __init__(self, state_tree, parent_state=None):
         self.tree = state_tree
         """The stree definition of this state."""
         self.parent_state = parent_state
         """The previous state. If we leave this state, we'll come back
         to this parent state."""
-        self.navigator = navigator
-        """Game states must provide a viewport."""
-        self.name = self.tree.get('name', '')
-        """States must have a name, so it can be displayed"""
+        try:
+            self.name = self.tree['name']
+            """States must have a name, so it can be displayed"""
+        except:
+            raise InvalidTreeException('State defined by tree %s has no name.'
+                                       % self.tree)
+        """Scape states must provide a viewport."""
         self.action = self.tree.get('action', '')
         """Actions are associated to a main action."""
-        self.actions = state_tree.get('actions')
-        """And provide access to sub-actions."""
-        self.sub_object = None
+        self.actions = state_tree.get('actions', {})
+
+    def receive(self, event):
+        pass
+
+    def pick_substate(self, event_data):
+        """
+        If the event_data is associated to a substate, return the substate.
+        """
+        substate = self.actions.get(event_data)
+        if substate:
+            bus.bus.publish(substate,
+                            bus.NEW_STATE)
+            self.navigator.set_char()
+        return substate
+
+
+class ScapeState(object):
+    def __init__(self, state_tree, parent_state=None, scape=None):
+        self.scape = scape
         """Sub actions can have a complement, the sub_object."""
-        self.sub_object_display = None
-        """This sub_object should also have a way to be displayed."""
         self.pauses_game = state_tree.get('pauses_game', True)
         """By default, every state pauses game but the main state."""
 
     def deactivate(self):
+        """Actions to take when leaving state."""
         pass
 
     def activate(self):
-        self.sub_object = None
+        """Actions to take when entering state."""
+        pass
 
-    def change_sub_object_display(self):
-        if not isinstance(self.sub_object, int):
-            if self.sub_object.is_multi_tile():
-                self.navigator.set_multi_char(self.sub_object.character,
-                                              self.sub_object.width,
-                                              self.sub_object.height)
-            else:
-                self.navigator.set_char(self.sub_object.character)
+    def dispatch_input_event(self, event_data):
+        """React to a simple input. Very often, it will mean
+        passing the input to the scape."""
+        pass
+
+    def dispatch_selection_event(self, event_data):
+        """React to a "confirmation" event."""
 
     def receive(self, event):
-        def pick_substate(event_data):
-            if self.actions:
-                substate = self.actions.get(event_data)
-                if substate:
-                    bus.bus.publish(substate,
-                                    bus.NEW_STATE)
-                    self.navigator.set_char()
-                return substate
-
-        def pick_subobject(event_data):
-            subobject = self.tree.get('submenu', {}).get(event_data)
-            if subobject:
-                self.sub_object = subobject.get('subobject')
-                self.sub_object_display = subobject.get('display')
-                self.change_sub_object_display()
-
-            return subobject
-
-        def check_pause(event_data):
-            if event_data == Inputs.SPACE:
-                self.pauses_game = not self.pauses_game
-
         event_data = event.get('data')
-        if (event.get('type') == bus.INPUT_EVENT):
-            if not (pick_substate(event_data) or
-                    pick_subobject(event_data) or
-                    check_pause(event_data) or
-                    self._check_for_previous_state(event_data))\
-                    and self.navigator:
-                self.navigator.receive(event_data)
-        elif (event.get('type') == bus.AREA_SELECT):
+        event_type = event.get('type')
+        if event_type == bus.LEAVE_EVENT:
+            self.check_for_previous_state()
+        if event_type == bus.INPUT_EVENT:
+            self.dispatch_input_event()
+        elif event_type == bus.AREA_SELECT:
+            self.dispatch_selection_event()
             need_subobject = (bool(self.tree.get('submenu')) and
                               self.sub_object is None)
             if not need_subobject:
@@ -97,26 +116,15 @@ class GameState(object):
                 bus.bus.publish('Pick between ' + ', '.join(keys))
 
     def send_command(self, area):
-        if self.action == Actions.BUILD:
-            command = BuildCommand(area)
-        elif self.action == Actions.PUT:
-            command = PutCommand(area, self.sub_object)
-            self.change_sub_object_display()
-        elif self.action == Actions.ROOMS:
-            command = RoomCommand(area, self.sub_object)
-        if command is not None:
-            bus.bus.publish({'command': command}, bus.WORLD_EVENT)
+        pass
 
-    def _check_for_previous_state(self, event_data):
+    def check_for_previous_state(self, event_data):
         """
         Go to previous state if and only if :
-            - The escape key has been hit
-            - There is no current selection in the navigator
+            - There is no current selection in the scape
             - There is a parent state
         """
-        if event_data == Inputs.ESCAPE and\
-            not self.navigator.selection and\
-                self.parent_state is not None:
+        if not self.navigator.selection and self.parent_state is not None:
             bus.bus.publish(self.parent_state, bus.PREVIOUS_STATE)
             self.navigator.set_char()
             return True
