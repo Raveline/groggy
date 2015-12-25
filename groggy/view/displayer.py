a8f93ea17fc24d07aa5a22d47dcac8926af3e130
Raveline <eraveline@gmail.com>
2015-12-25 10:17:10 +0100

    Put flush in displayer

--- groggy/view/displayer.py
+++ groggy/view/displayer.py
@@ -4,14 +4,18 @@ the current model and state into a view.
 The displayer should always be extended by client code.
 '''
 
+import libtcodpy as tcod
+
 
 class Displayer(object):
     def __init__(self, model):
         self.model = model
 
     def call(self, blink, state, consoles):
+        self.display(blink, state, consoles)
         for console in consoles.values():
             console.blit_on(0)
+        tcod.console_flush()
 
     def clip_world(self, world, clip_box):
         clipped_y = world[clip_box.y:clip_box.y + clip_box.h]
