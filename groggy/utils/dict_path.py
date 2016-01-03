a37b50d96aba9b50b59ec8ff4d9f8940a1939667
Raveline <eraveline@gmail.com>
2016-01-03 15:00:41 +0100

    Add new exceptions to dict_path for easier debug

--- groggy/utils/dict_path.py
+++ groggy/utils/dict_path.py
@@ -8,6 +8,9 @@ def read_path_dict(dic, key):
     """
     key_elements = key.split('.')
     current = dic
+    if current is None:
+        raise InvalidPathException('Received an empty dictionary when'
+                                   'looking for key %s' % key)
     for elem in key_elements[:-1]:
         current = current.get(elem)
         if current is None:
@@ -15,4 +18,9 @@ def read_path_dict(dic, key):
                 'Path was %s. Could not find key %s in dict %s.'
                 % (key, elem, dic)
             )
+    if key_elements is None:
+        raise InvalidPathException(
+            'Key is incorrect : %s. Started with %s to be found in %s'
+            % (key_elements, key, dic)
+        )
     return current[key_elements[-1]]
